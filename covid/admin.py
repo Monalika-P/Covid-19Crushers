e2c2bb9bd5bd0c39c390610fccd4566a8dde27b3
Monalika P <57065882+Monalika-P@users.noreply.github.com>
2020-04-10 23:58:53 +0530

    Add files via upload

---
+++ covid/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from covid.models import gethelp,positive,hel,symptoms
+# Register your models here.
+
+admin.site.register(gethelp)
+admin.site.register(positive)
+admin.site.register(hel)
+admin.site.register(symptoms)
